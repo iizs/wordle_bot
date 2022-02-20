dafd18f70893d040a47faf06973e8b09254598ff
Kirin Choi <choi.kirin@gmail.com>
2022-02-20 11:08:29 -0800

    Add FileHandler logger

--- run_random_bot.py
+++ run_random_bot.py
@@ -1,9 +1,17 @@
 from wordle.game import *
 from wordle.random_bot import RandomBotPlayer
 import logging
+import datetime
 
 FORMAT = '[%(asctime)s] %(levelname)s {%(filename)s:%(lineno)d} - %(message)s'
-logging.basicConfig(format=FORMAT, level=logging.INFO)
+logging.basicConfig(
+    format=FORMAT,
+    level=logging.INFO,
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(f'logs/run_random_bot_{datetime.datetime.now().strftime("%Y%m%d%H%M")}.log')
+    ]
+)
 
 player = RandomBotPlayer()
 
